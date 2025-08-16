9e4dccb9bd0fb5333efb772e43d511409ce74fe6
ebarrera-2021235 <ebarrera-2021235@kinal.edu.gt>
2025-08-15 23:29:56 -0600

    add: Crear conexion de la aplicacion hacia la BD en archivo config (conexion)

--- Velure_Concesionario/src/java/config/Conexion.java
+++ Velure_Concesionario/src/java/config/Conexion.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package config;
 
-/**
- *
- * @author USUARIO
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+
 public class Conexion {
+    Connection conexion;
     
+    public Connection Conexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost3306/DBConcesionarioIN5AM=false", "root", "admin");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return conexion;
+    }
 }
