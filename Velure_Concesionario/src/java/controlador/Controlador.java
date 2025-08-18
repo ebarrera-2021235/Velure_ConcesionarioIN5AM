005d8b41a851f108ff1718058b5b7cc815b2661e
egomez-2021272 <egomez-2021272@kinal.edu.gt>
2025-08-18 16:28:37 -0600

    add: Añadir parámetro para dirigr a la vista menu principal en Controlador.java

--- Velure_Concesionario/src/java/controlador/Controlador.java
+++ Velure_Concesionario/src/java/controlador/Controlador.java
@@ -10,12 +10,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Empleado;
+import modelo.EmpleadoDAO;
 
 /**
  *
- * @author USUARIO
+ * @author informatica
  */
 public class Controlador extends HttpServlet {
+    Empleado empleado = new Empleado();
+    EmpleadoDAO empleadoDAO = new EmpleadoDAO();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -28,18 +32,10 @@ public class Controlador extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controlador</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String menu = request.getParameter("menu");
+        String accion = request.getParameter("accion");
+        if(menu.equals("Principal")){
+            request.getRequestDispatcher("Principal.jsp").forward(request, response);
         }
     }
 
