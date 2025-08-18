9c3e4f6a29f87946d86821fa90e5f73ed21bb184
egomez-2021272 <egomez-2021272@kinal.edu.gt>
2025-08-18 16:23:44 -0600

    fix: Corregir validación de correo empleado y verificar que exista en RegistrarUsuario.java

--- Velure_Concesionario/src/java/controlador/RegistrarUsuario.java
+++ Velure_Concesionario/src/java/controlador/RegistrarUsuario.java
@@ -6,10 +6,12 @@ package controlador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 import modelo.Empleado;
 import modelo.EmpleadoDAO;
 import modelo.Usuario;
@@ -34,19 +36,7 @@ public class RegistrarUsuario extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RegistrarUsuario</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RegistrarUsuario at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+      
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -87,6 +77,7 @@ public class RegistrarUsuario extends HttpServlet {
         }
         
         //aqui se busca
+       // int codigoEmpleado = Integer.parseInt(request.getParameter("codigoEmpleado"));
         Empleado empleado = empleadoDAO.validar(correoUsuario);
         
         if(empleado.getCodigoEmpleado() <= 0){
@@ -127,7 +118,6 @@ public class RegistrarUsuario extends HttpServlet {
             request.getRequestDispatcher("Registrarse.jsp").forward(request, response);
 
         }
-        
         
     }
 
