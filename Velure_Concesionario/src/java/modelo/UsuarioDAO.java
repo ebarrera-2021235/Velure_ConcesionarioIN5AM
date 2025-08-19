f017e46228acbfab6c6aa8434e6ccc4e13260993
ebarrera-2021235 <ebarrera-2021235@kinal.edu.gt>
2025-08-18 21:42:06 -0600

    refactor: Corregir contraseña de conexion

--- Velure_Concesionario/src/java/modelo/UsuarioDAO.java
+++ Velure_Concesionario/src/java/modelo/UsuarioDAO.java
@@ -36,6 +36,41 @@ public class UsuarioDAO {
     }
     
     public int agregar(Usuario user){
+        // Grabar Empleado
+        String estadoEmpleado = "1"; // dato quemado
+        String sqlEmpleado = "insert into Empleados "
+                + "(nombresEmpleado, DPIEmpleado, estado, telefonoEmpleado, correoEmpleado) "
+                + "values(?, ?, ?, ?, ?)";
+        
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sqlEmpleado);
+            ps.setString(1, user.getNombresUsuario());
+            ps.setString(2, user.getDPIEmpleado());
+            ps.setString(3, estadoEmpleado);
+            ps.setString(4, user.getTelefonoEmpleado());
+            ps.setString(5, user.getCorreoUsuario());
+            ps.executeUpdate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        // Recuperar ID del Empleado
+        String sqlIdEmpleado = "select codigoEmpleado from Empleados where correoEmpleado = ?";
+        int codEmpleado = 0; // se inicializa para asegurarme que no guarde el último valor
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sqlIdEmpleado);
+            ps.setString(1, user.getCorreoUsuario());
+            rs = ps.executeQuery();
+            while(rs.next()){
+                codEmpleado=(rs.getInt("codigoEmpleado"));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        // Grabar el usuario
         String sql = "insert into usuarios (nombresUsuario, userName, contrasenia, correoUsuario, codigoEmpleado) values (?, ?, ?, ?, ?)";
         try{
             con = cn.Conexion();
@@ -44,7 +79,7 @@ public class UsuarioDAO {
             ps.setString(2, user.getUserName());
             ps.setString(3, user.getContrasenia());
             ps.setString(4, user.getCorreoUsuario());
-            ps.setInt(5, user.getCodigoEmpleado());
+            ps.setInt(5, codEmpleado);
             return ps.executeUpdate();
         }catch(Exception e){
             e.printStackTrace();
