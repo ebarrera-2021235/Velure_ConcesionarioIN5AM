867c245f7b52915d1f437728995383001de43a8f
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2025-08-19 22:44:55 -0600

    add: Agregar Validar a ClienteDAO.java

--- Velure_Concesionario/src/java/modelo/ClienteDAO.java
+++ Velure_Concesionario/src/java/modelo/ClienteDAO.java
@@ -12,6 +12,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteDAO {
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int resp;
+    
+    public Cliente validar(String nombresCliente, String telefonoCliente) {
+        Cliente clien = new Cliente();
+        String sql = "Select * from Clientes where nombresCliente = ? and telefonoCliente = ?";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, nombresCliente);
+            ps.setString(2, telefonoCliente);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                clien.setCodigoCliente(rs.getInt(1));
+                clien.setNombresCliente(rs.getString(2));
+                clien.setDPICliente(rs.getString(3));
+                clien.setDireccionCliente(rs.getString(4));
+                clien.setEstado(rs.getString(5));
+                clien.setTelefonoCliente(rs.getString(6));
+                clien.setCorreoCliente(rs.getString(7));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return clien;
+    }
+    
+    
     
     
     
