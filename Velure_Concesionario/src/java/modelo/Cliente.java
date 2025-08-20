1834879445f0acf51168c49d9f26db303fbeec06
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2025-08-19 22:38:51 -0600

    add: Agregar Getter y Setter a Cliente.java

--- Velure_Concesionario/src/java/modelo/Cliente.java
+++ Velure_Concesionario/src/java/modelo/Cliente.java
@@ -23,6 +23,63 @@ public class Cliente {
         this.telefonoCliente = telefonoCliente;
         this.correoCliente = correoCliente;
     }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNombresCliente() {
+        return nombresCliente;
+    }
+
+    public void setNombresCliente(String nombresCliente) {
+        this.nombresCliente = nombresCliente;
+    }
+
+    public String getDPICliente() {
+        return DPICliente;
+    }
+
+    public void setDPICliente(String DPICliente) {
+        this.DPICliente = DPICliente;
+    }
+
+    public String getDireccionCliente() {
+        return direccionCliente;
+    }
+
+    public void setDireccionCliente(String direccionCliente) {
+        this.direccionCliente = direccionCliente;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefonoCliente() {
+        return telefonoCliente;
+    }
+
+    public void setTelefonoCliente(String telefonoCliente) {
+        this.telefonoCliente = telefonoCliente;
+    }
+
+    public String getCorreoCliente() {
+        return correoCliente;
+    }
+
+    public void setCorreoCliente(String correoCliente) {
+        this.correoCliente = correoCliente;
+    }
+    
     
     
     
