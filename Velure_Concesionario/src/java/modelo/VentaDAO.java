b9437ab8fdfea66c520122ba980e3b6e92524300
ecatalan-2024133 <ecatalan-2024133@kinal.edu.gt>
2025-08-21 19:36:32 -0600

    add: Añadir variables y método de listar en VentaDAO.java

--- Velure_Concesionario/src/java/modelo/VentaDAO.java
+++ Velure_Concesionario/src/java/modelo/VentaDAO.java
@@ -4,10 +4,48 @@
  */
 package modelo;
 
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Roberto Catalán
  */
 public class VentaDAO {
-    
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int resp;
+
+
+ //MÉTODOS DEL CRUD 
+ //LISTAR
+    public List listar(){
+        String sql = "select * from ventas";
+        List<Venta> listaVenta = new ArrayList<>();
+        try{
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Venta ven = new Venta();
+                ven.setCodigoVenta(rs.getInt(1));
+                ven.setFecha(rs.getDate(2));
+                ven.setTotal(rs.getDouble(3));
+                ven.setDescripcion(rs.getString(4));
+                ven.setTipoDePago(rs.getString(5));
+                ven.setCodigoCliente(rs.getInt(6));
+                ven.setCodigoEmpleado(rs.getInt(7));
+                listaVenta.add(ven);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return listaVenta;
+    } 
 }
